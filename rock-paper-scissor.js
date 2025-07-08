b98f51e243e57b3b404ca73bc37714edf2300531
Ahaan Shah <1ahaanshah@gmail.com>
2025-07-08 18:20:38 +0530

    Got the game working - learned how to define and call functions

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,6 +1,43 @@
 // console.log("Hello World!");
 
 const button = document.querySelector(".get-input");
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
+
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    if(humanScore > computerScore){
+        alert("You Win! Congrats lol");
+    } else if(humanScore > computerScore){
+        alert("You Lose! Get good noob");
+    } else {
+        alert("Draw!");
+    }
+};
+
+function playRound(humanChoice, computerChoice) {
+    let hc = humanChoice.toLowerCase();
+    let cc = computerChoice.toLowerCase();
+    if ((hc === "rock" && cc === "paper") || (hc === "paper" && cc === "scissors") || (hc === "scissors" && cc === "rock")) {
+        computerScore = computerScore + 1;
+        alert(`You Lose! ${computerChoice} beats ${humanChoice}!`);
+    }
+    else if ((hc === "rock" && cc === "paper") || (hc === "paper" && cc === "scissors") || (hc === "scissors" && cc === "rock")) {
+        humanScore = humanScore + 1;
+        alert(`You Win! ${humanChoice} beats ${computerChoice}!`);
+    }
+    else {
+        alert(`Draw!`);
+    }
+};
 
 function getComputerChoice() {
     let x = Math.random();
