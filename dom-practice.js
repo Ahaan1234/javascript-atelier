234f84d017766ec67f495d0dc7f49d8dd0716374
Ahaan Shah <1ahaanshah@gmail.com>
2025-07-09 17:48:15 +0530

    tried bigger range of elements

--- dom-practice.js
+++ dom-practice.js
@@ -6,7 +6,16 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 const paragraph = document.createElement("p");
-paragraph.style.color("red")
-paragraph.textContent("Hey I'm red!")
+paragraph.style.color = "red";
+paragraph.textContent = "Hey I'm red!";
 
-container.appendChild(content);
+const header = document.createElement("h3");
+header.style.color = "blue";
+header.textContent = "Hey I'm a blue h3!";
+
+const box = document.createElement("div");
+box.style.cssText = "border: 4px solid black; background-color: pink";
+box.innerHTML = "<h1> I'm in a div</h1> <p>ME TOO!</p>";
+
+container.append(content, paragraph, header, box);
+
